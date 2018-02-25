5cbeda4fb08dfb5a8dbaaf541880b9aa9e356082
Viyi <arthur.gho.howard@gmail.com>
2018-02-24 23:14:32 -0600

    Baby Steps
    It opens pokemon showdown now, doesn't click yet though.

--- src/main/java/pokebot/pokeplayer.java
+++ src/main/java/pokebot/pokeplayer.java
@@ -1,7 +1,9 @@
 package pokebot;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import me.sargunvohra.lib.pokekotlin.client.PokeApi;
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
@@ -12,10 +14,15 @@ import me.sargunvohra.lib.pokekotlin.model.PokemonSpeciesDexEntry;
 public class pokeplayer {
 	
 	public static void main(String[]args) {
-		//WebDriver driver = new FirefoxDriver();
+		//set this to your gecko driver path
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Viyi\\Desktop\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		
-		String rivalPoke = "charizard";
-		PokeApi pokeApi = new PokeApiClient();
+		
+		//Open pokemon showdown, and login
+		driver.get("http://play.pokemonshowdown.com/");
+		
+		driver.findElement(By.cssSelector(".userbar > button:nth-child(1)")).click();
 		
 		
 	}
