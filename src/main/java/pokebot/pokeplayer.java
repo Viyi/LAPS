b9ba157931e3fd96342385eebf3ff09b51f3556b
Viyi <arthur.gho.howard@gmail.com>
2018-02-24 22:03:40 -0600

    Initial Commit

---
+++ src/main/java/pokebot/pokeplayer.java
@@ -0,0 +1,23 @@
+package pokebot;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+import me.sargunvohra.lib.pokekotlin.model.Pokemon;
+import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
+import me.sargunvohra.lib.pokekotlin.model.PokemonSpeciesDexEntry;
+
+public class pokeplayer {
+	
+	public static void main(String[]args) {
+		//WebDriver driver = new FirefoxDriver();
+		
+		String rivalPoke = "charizard";
+		PokeApi pokeApi = new PokeApiClient();
+		
+		
+	}
+
+}
